3316c5330c3c3697b396aaffe0c7ad8e7faa0ab0
ringdinglinn <lalaponii@gmail.com>
2022-09-13 15:52:50 +0200

    generated

--- dist/_nuxt/static/1663073319/posts/hello-hello-hello/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663073319",layout:"default",error:a,state:{posts:[]},serverRendered:true,routePath:"\u002Fposts\u002Fhello-hello-hello",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},http:{browserBaseURL:b}}}}(null,"\u002F"));
