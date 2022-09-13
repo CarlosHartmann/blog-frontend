602df4d91b5a164db3e7e07629b47211b2703774
ringdinglinn <lalaponii@gmail.com>
2022-09-13 16:53:19 +0200

    contact form ready

--- pages/thankyou/index.vue
+++ pages/thankyou/index.vue
@@ -1,3 +1,29 @@
 <template>
-    <h2>Thank you!</h2>
+    <div class="container grid main-grid">
+        <back-button class="back-button"></back-button>
+        <h3 class="thank-you">Thank you!</h3>
+    </div>
 </template>
+
+<style scoped lang="scss">
+    .back-button {
+        grid-column:1;
+        grid-row:1;
+    }
+
+    .thank-you {
+        grid-column: 2;
+        grid-row:1;
+    }
+</style>
+
+<script>
+    import BackButton from '~/components/BackButton.vue'
+
+    export default {
+        layout: 'ThankYou',
+        components: {
+            BackButton
+        }
+    }
+</script>
